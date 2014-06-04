b078c2c48edc1bbf6de0e09bd54ec0c2589d6860
bill <vanbesien@gmail.com>
2014-06-04 00:14:47 -0400

    Added new templates folder. Want to try experimenting with compiling libs to shared libs as well as static ones

---
+++ templates/sample/include/sample.h
@@ -0,0 +1,9 @@
+#ifndef _SAMPLE_H_
+#define _SAMPLE_H_
+
+#define SAMPLE_MAXLEN (80)
+
+void
+SAMPLE_helloworld(void);
+
+#endif /* _SAMPLE_H_ */
---
+++ templates/sample/src/sample.c
@@ -0,0 +1,9 @@
+#include <sample.h>
+
+#include <stdio.h>
+
+void SAMPLE_helloworld(void)
+{
+
+    printf("From sample lib: Hello, world!\n");
+}
